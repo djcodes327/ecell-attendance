92d9269444610a119146b90540e75dccdd68878d
Deep Joshi <djoshi27022001@gmail.com>
2021-03-13 23:38:26 +0530

    Domains Added

--- ecell_attendance_backend/settings.py
+++ ecell_attendance_backend/settings.py
@@ -5,7 +5,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -13,10 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'k5$&7&9ofe2jwo#)(+s5(dq$48!3-)+&4!h&*c32^+rj-7(2p)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['django-djcodes.herokuapp.com','127.0.0.1']
-
+ALLOWED_HOSTS = ['django-djcodes.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -49,8 +47,7 @@ ROOT_URLCONF = 'ecell_attendance_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,7 +62,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecell_attendance_backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -75,7 +71,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -95,7 +90,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-X_FRAME_OPTIONS='SAMEORIGIN' # only if django version >= 3.0
+X_FRAME_OPTIONS = 'SAMEORIGIN'  # only if django version >= 3.0
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
@@ -109,7 +104,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
